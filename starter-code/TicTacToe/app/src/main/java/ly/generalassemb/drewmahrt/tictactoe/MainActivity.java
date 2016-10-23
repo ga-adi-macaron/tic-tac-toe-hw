2b716c077ae38e6cde5d8c3c6695ceb72f9a40e3
NikShuvalov <nshuvalov89@gmail.com>
2016-10-23 17:41:57 -0400

    Added SQLdatabase. Have not tested yet, but the code should be inputting the data. Will debug, if necessary, once I start calling from the database.

--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -1,6 +1,7 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,15 +18,27 @@ public class MainActivity extends AppCompatActivity {
     public static final int OUTCOME_CODE = 2369;
     public static final int WINNER_CODE= 1337;
     public static final int DRAW_CODE = 424;
+    SQLiteDatabase gameHistoryDB;
+    Cursor c;
+
+    //ToDo: Make a recyclerView that displays multiple lines of winners.
+    //ToDo: Add results to dataBase.
+    //ToDo: Add an activity to display gamestate results of previous games.
+    //ToDo: Load data from database.
+    //ToDo: Clear data from database.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        SQLiteDatabase gameHistoryDB = this.openOrCreateDatabase("Game_History", MODE_PRIVATE, null);
-//
-//        gameHistoryDB.execSQL("CREATE TABLE IF NOT EXISTS game_history (gameID INTEGER PRIMARY KEY, winnerName VARCHAR, loserName VARCHAR)");
+        gameHistoryDB = this.openOrCreateDatabase("Game_History", MODE_PRIVATE, null);
+
+        gameHistoryDB.execSQL("CREATE TABLE IF NOT EXISTS game_history (gameID INTEGER PRIMARY KEY, p1Name VARCHAR, p2Name VARCHAR, winner VARCHAR, gameState VARCHAR)");
+        //If game was draw winner var is "draw"
+        c = gameHistoryDB.rawQuery("SELECT * FROM game_history", null);
+
 
 
 
@@ -59,11 +72,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == OUTCOME_CODE){
+
+            String p1Name = data.getStringExtra("Player1Name");
+            String p2Name = data.getStringExtra("Player2Name");
+            String winner = data.getStringExtra("winner");
+            int[] gameState = data.getIntArrayExtra("gameState");
+            String gameStateText="";
+            for (Integer i:gameState) {
+                gameStateText+=Integer.toString(i);
+            }
+
+            //This gives Index of items when I'm looking for the data.
+//            int p1Index = c.getColumnIndex("p1Name");
+//            int p2Index = c.getColumnIndex("p2Name");
+//            int winnerIndex = c.getColumnIndex("winner");
+//            int gameStateIndex = c.getColumnIndex("gameState");
+
+            String sqlExecutionString = "INSERT INTO game_history (p1Name, p2Name, winner,gameState) VALUES ("+p1Name+", "+p2Name+", "+ winner+", "+ gameStateText+")";
+
             if(resultCode == DRAW_CODE){
                 lastWinText.setText("Previous Winner: \n"+"Ended in a draw");
+
             }else if (resultCode == WINNER_CODE){
                 lastWinText.setText("Previous Winner: \n"+ data.getStringExtra("winner"));
+
             }
+            gameHistoryDB.execSQL(sqlExecutionString);
         }
 
     }
