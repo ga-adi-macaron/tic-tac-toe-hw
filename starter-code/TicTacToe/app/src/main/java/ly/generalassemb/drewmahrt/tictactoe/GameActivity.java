8c4a85e7ad7a6c786a251c49c83b14dc652c7fdb
KorbKenny <korbkenny@gmail.com>
2016-10-23 15:40:17 -0400

    Can play one game, no winner

--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,13 +1,83 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView mMessage;
+    //TextView q1,q2,q3,q4,q5,q6,q7,q8,q9;
+    //ArrayList<TextView> mSquares;
+    int i;
+    String p1, p2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        i = 0;
+
+        Intent iFrom = getIntent();
+        p1 = iFrom.getStringExtra("1");
+        p2 = iFrom.getStringExtra("2");
+
+//        mSquares.add((TextView) findViewById(R.id.textView));
+//        mSquares.add((TextView) findViewById(R.id.textView2));
+//        mSquares.add((TextView) findViewById(R.id.textView3));
+//        mSquares.add((TextView) findViewById(R.id.textView4));
+//        mSquares.add((TextView) findViewById(R.id.textView5));
+//        mSquares.add((TextView) findViewById(R.id.textView6));
+//        mSquares.add((TextView) findViewById(R.id.textView7));
+//        mSquares.add((TextView) findViewById(R.id.textView8));
+//        mSquares.add((TextView) findViewById(R.id.textView9));
+
+        mMessage = (TextView)findViewById(R.id.game_message_text);
+        mMessage.setText("It is " + p1 + "'s turn! (X)");
+
+    }
+
+    public void clickThatButton(View v){
+        if(i % 2 == 0){
+            if(   ((TextView)findViewById(v.getId())).getText().toString().equals("")  ) {
+
+
+                ((TextView) findViewById(v.getId())).setText("X");
+                mMessage.setText("It is " + p2 + "'s turn! (O)");
+                i++;
+            }
+        }
+        else{
+            if(  ((TextView)findViewById(v.getId())).getText().toString().equals("")   ){
+                ((TextView)findViewById(v.getId())).setText("O");
+                mMessage.setText("It is " + p1 + "'s turn! (X)");
+                i++;
+            }
+
+
+            }
+
     }
 }
+
+
+//        q1 = (TextView)findViewById(R.id.textView);
+//        q2 = (TextView)findViewById(R.id.textView2);
+//        q3 = (TextView)findViewById(R.id.textView3);
+//        q4 = (TextView)findViewById(R.id.textView4);
+//        q5 = (TextView)findViewById(R.id.textView5);
+//        q6 = (TextView)findViewById(R.id.textView6);
+//        q7 = (TextView)findViewById(R.id.textView7);
+//        q8 = (TextView)findViewById(R.id.textView8);
+//        q9 = (TextView)findViewById(R.id.textView9);
--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -5,13 +5,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText mPlayerOneEdit, mPlayerTwoEdit;
+    Button mPlay;
+    TextView mLastWinnter;
+    String mP1, mP2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Reference
+        mPlayerOneEdit = (EditText) findViewById(R.id.player_one_name);
+        mPlayerTwoEdit = (EditText) findViewById(R.id.player_two_name);
+        mPlay = (Button) findViewById(R.id.start_game_button);
+        mLastWinnter = (TextView) findViewById(R.id.last_winner_text);
+
+
+
+        //Clicking button, get text from EditTexts if they're not blank
+        mPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mP1 = mPlayerOneEdit.getText().toString();
+                mP2 = mPlayerTwoEdit.getText().toString();
+
+                if(mP1.trim().equals("")) {
+                    mPlayerOneEdit.setError("HI");
+                }
+                if(mP2.trim().equals("")) {
+                    mPlayerTwoEdit.setError("Hello");
+                }
+
+                if(mP1.trim().equals("")||mP2.trim().equals("")) {
+
+                }
+                else{
+                    Intent i = new Intent(MainActivity.this, GameActivity.class);
+                    i.putExtra("1", mP1);
+                    i.putExtra("2", mP2);
+                    startActivity(i);
+                    }
+                }
+            });
+
+
+            //Recieve data from the game after it's over
+            //Don't use the normal send intent, it's something else, I forget
+
+        }
 }
