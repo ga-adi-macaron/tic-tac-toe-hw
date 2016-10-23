cd550a906b06ef5c7d175c44964e050fe519eafb
cbrads61 <cbrads61@gmail.com>
2016-10-23 12:48:16 -0400

    Main Activity can get player names and send them to game activity

--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,13 +1,33 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class GameActivity extends AppCompatActivity  {
+
+    private TextView mWhosTurn;
+
+    private String mP1Name;
+
+    private String mP2Name;
+
+ 
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent recieveNameIntent = getIntent();
+        mP1Name = recieveNameIntent.getStringExtra("P1Name");
+        mP2Name = recieveNameIntent.getStringExtra("P2Name");
+
     }
+
 }
--- starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ starter-code/TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -5,13 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPlayer1Name;
+
+    private EditText mPlayer2Name;
+
+    private Button mStartButton;
+
+    public static final int REQUEST = 61;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayer1Name = (EditText) findViewById(R.id.player_one_name);
+        mPlayer2Name = (EditText) findViewById(R.id.player_two_name);
+        mStartButton = (Button) findViewById(R.id.start_game_button);
+
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayer1Name.getText().toString().isEmpty()) {
+                    mPlayer1Name.setError("Please Enter Name for Player 1");
+                }
+                else if (mPlayer2Name.getText().toString().isEmpty()){
+                    mPlayer2Name.setError("Please Enter Name for Player 2");
+                }
+                else{
+                    Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
+                    startGameIntent.putExtra("P1Name", mPlayer1Name.getText().toString());
+                    startGameIntent.putExtra("P2Name", mPlayer2Name.getText().toString());
+                    startActivityForResult(startGameIntent, REQUEST);
+                }
+            }
+        });
     }
 }
